83fba3ab747d86a2b0e7338088291e1a6bffa870
D Tucker <danielle@tuckerlabs.com>
2013-08-22 10:26:34 -0700

    Working without AWS

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+require 'carrierwave/processing/mime_types'
 
 class ImageUploader < CarrierWave::Uploader::Base
 
@@ -7,13 +8,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-   storage :file
+  # storage :file
   #storage :fog
+
+  include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # if AWS
+    #""
+     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
